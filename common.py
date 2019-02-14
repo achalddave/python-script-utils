4ac1053f8c1196d4657a8cf072cb891bcb45aa59
Achal Dave <achalddave@live.com>
2019-02-14 13:40:11 -0500

    Initial commit with common setup

---
+++ common.py
@@ -0,0 +1,55 @@
+import utils.log as log_utils
+import logging
+import subprocess
+from pathlib import Path
+
+
+def _to_path(x):
+    return x if isinstance(x, Path) else Path(x)
+
+
+def common_setup(script_name,
+                 output_dir,
+                 args=None,
+                 logfile_prefix=None,
+                 save_git_state=True,
+                 git_state_dir='git-state'):
+    """Common setup for scripts that output to a directory.
+    - Setup logging
+    - Save git state
+    - Log args to stdout and logging path
+    Args:
+        script_name (str): Script name. If this is a path (e.g. __file__), we
+            use just the stem.
+        output_dir (Path)
+        args: Output of parser.parse_args()
+        logfile_prefix (str): Specify prefix for logging file. The logging file
+            path is output_dir / (logfile_prefix + time + '.log'), where
+            logfile_prefix by default is `script_name`.
+        save_git_state (bool): Whether to save the git state.
+    """
+
+    output_dir = _to_path(output_dir)
+    if not isinstance(output_dir, Path):
+        output_dir = Path(output_dir)
+
+    name = Path(script_name).stem
+    if logfile_prefix is None:
+        logfile_prefix = name + '.log'
+    else:
+        logfile_prefix = Path(logfile_prefix)
+        if logfile_prefix.suffix != '.log':
+            logfile_prefix = logfile_prefix.with_suffix(logfile_prefix.suffix +
+                                                        '.log')
+    log_file = log_utils.add_time_to_path(output_dir / logfile_prefix)
+    log_utils.setup_logging(log_file)
+
+    if save_git_state:
+        subprocess.call([
+            git_state_dir + '/save_git_state.sh',
+            log_file.with_suffix('.git-state')
+        ])
+
+    if args is not None:
+        import pprint
+        logging.info('Args:\n%s', pprint.pformat(vars(args)))
---
+++ log.py
@@ -0,0 +1,49 @@
+import logging
+from datetime import datetime
+from pathlib import Path
+
+
+def add_time_to_path(logging_filepath):
+    logging_filepath = Path(logging_filepath)
+    now = datetime.now().strftime('%b%d-%H-%M-%S')
+    return logging_filepath.with_name(logging_filepath.stem + '_' + now +
+                                      logging_filepath.suffix)
+
+
+def setup_logging(logging_filepath):
+    """Setup root logger to log to file and stdout.
+    All calls to logging will log to `logging_filepath` as well as stdout.
+    Also creates a file logger that only logs to , which can
+    be retrieved with logging.getLogger(logging_filepath).
+    Args:
+        logging_filepath (str): Path to log to.
+    """
+    if isinstance(logging_filepath, Path):
+        logging_filepath = str(logging_filepath)
+
+    log_format = ('%(asctime)s %(filename)s:%(lineno)4d: ' '%(message)s')
+    stream_date_format = '%H:%M:%S'
+    file_date_format = '%m/%d %H:%M:%S'
+
+    # Clear any previous changes to logging.
+    logging.root.handlers = []
+    logging.root.setLevel(logging.INFO)
+
+    file_handler = logging.FileHandler(logging_filepath)
+    file_handler.setFormatter(
+        logging.Formatter(log_format, datefmt=file_date_format))
+    logging.root.addHandler(file_handler)
+
+    # Logger that logs only to file. We could also do this with levels, but
+    # this allows logging specific messages regardless of level to the file
+    # only (e.g. to save the diff of the current file to the log file).
+    file_logger = logging.getLogger(logging_filepath)
+    file_logger.addHandler(file_handler)
+    file_logger.propagate = False
+
+    console_handler = logging.StreamHandler()
+    console_handler.setFormatter(
+        logging.Formatter(log_format, datefmt=stream_date_format))
+    logging.root.addHandler(console_handler)
+
+    logging.info('Writing log file to %s', logging_filepath)
